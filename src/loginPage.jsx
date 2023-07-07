9c285484d5e05a89a8fea7733e44664641732ae2
Jiamin <267354491@qq.com>
2023-07-07 14:51:47 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from 'react'
 import { supabase } from './supabaseClient'
-import { useState } from 'react';
-import { Container, Heading, Text, Input, Button, ChakraProvider, extendTheme, CSSReset, Center } from "@chakra-ui/react";
+import { Container, Heading, Text, Input, Button, Flex, Box, ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 
 const customTheme = extendTheme({
@@ -11,38 +11,38 @@ const customTheme = extendTheme({
 });
 
 export default function Login() {
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
   const navigate = useNavigate();
   const [isSent, setIsSent] = useState(false);
 
   const handleLogin = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    setLoading(true);
-    const { error } = await supabase.auth.signInWithOtp({ email });
+    setLoading(true)
+    const { error } = await supabase.auth.signInWithOtp({ email })
 
     if (error) {
-      alert(error.error_description || error.message);
+      alert(error.error_description || error.message)
     } else {
-      alert('Check your email for the login link!');
-      navigate('/home');
+      alert('Check your email for the login link!')
+      navigate('/home')
     }
-    setLoading(false);
-  };
-
+    setLoading(false)
+  }
+  
   return (
     <ChakraProvider theme={customTheme}>
-      <CSSReset />
-      <Center>
-        <Container>
-          <Heading my="30px" color="blue.300">
-            Stay The Course
-          </Heading>
-          <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">
-            Sign in via magic link with your email below
-          </Text>
-          <form className="form-widget" onSubmit={handleLogin}>
+    <CSSReset />
+      <Flex justifyContent="center" alignItems="center" height="100%">
+          <Container>
+            <Heading my="30px" color="blue.300">Stay The Course</Heading>
+            <Flex flexDirection="column" alignItems="center">
+                <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">Sign in via magic link with your email below</Text>
+              </Flex>  
+        <form className="form-widget" onSubmit={handleLogin} style={{ textAlign: 'center' }}>
+        <Box marginBottom="20px" width="100%">
+          <div>
             <Input
               className="inputField"
               type="email"
@@ -51,12 +51,16 @@ export default function Login() {
               required={true}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <Button className={'button block'} disabled={loading} mt="20px">
+          </div>
+          </Box>
+          <div>
+            <Button className={'button block'} disabled={loading}>
               {loading ? <span>Loading</span> : <span>Send magic link</span>}
             </Button>
-          </form>
+          </div>
+        </form>
         </Container>
-      </Center>
+        </Flex>
     </ChakraProvider>
-  );
-}
+  )
+} 
