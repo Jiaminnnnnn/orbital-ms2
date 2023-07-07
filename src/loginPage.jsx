3bfb9e5515bee47f1d07607433bfb310ca0d8729
Jiamin <267354491@qq.com>
2023-07-07 23:57:41 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -33,8 +33,8 @@ export default function Login() {
   
   return (
     <ChakraProvider theme={customTheme}>
-    <Center h="100vh" bg="blue.300">
-    <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md" h="100%">
+    <Center h="1000vh" bg="blue.300">
+    <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md">
     <CSSReset />
       <Flex justifyContent="center" alignItems="center" height="100%">
           <Container>
