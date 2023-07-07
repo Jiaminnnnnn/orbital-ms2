49abecb37440ce2deaf098affc7ce19c47db5730
Jiamin <267354491@qq.com>
2023-07-07 11:02:16 +0800

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { supabase } from './supabaseClient';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './loginPage';
-import Profile from './profilePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Profile from './Profile';
 
-function App() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
+export default function App() {
   return (
-    <ChakraProvider>
-      <div className="container" style={{ padding: '50px 0 100px 0' }}>
-        <Router>
-          <Routes>
-            <Route exact path="/">
-              {!session ? <Login /> : <Profile key={session.user.id} session={session} />}
-            </Route>
-            <Route path="/profile">
-              <Profile key={session?.user.id} session={session} />
-            </Route>
-          </Routes>
-        </Router>
-      </div>
-    </ChakraProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
