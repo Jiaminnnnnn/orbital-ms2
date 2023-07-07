23f8c5c54eee4455026ac6eaac7a9effb701cee5
Jiamin <267354491@qq.com>
2023-07-07 10:42:54 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -12,6 +12,7 @@
   export default function Login() {
     const [loading, setLoading] = useState(false)
     const [email, setEmail] = useState('')
+    const history = useHistory();
 
     const handleLogin = async (event) => {
       event.preventDefault()
@@ -23,6 +24,7 @@
         alert(error.error_description || error.message)
       } else {
         alert('Check your email for the login link!')
+        history.pushState('/home');
       }
       setLoading(false)
     }
