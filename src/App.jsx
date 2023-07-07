e624fb702f8f4867297c46710f482812ab225be0
Jiamin <267354491@qq.com>
2023-07-07 11:31:00 +0800

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import { supabase } from './supabaseClient';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './loginPage';
-import Profile from './profilePage';
-import Home from './homePage';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './LoginPage';
+import Profile from './ProfilePage';
+import HomePage from './HomePage';
 
 function App() {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+    // Your session initialization code
   }, []);
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <Router>
         <Routes>
-          <Route exact path="/">
-            {!session ? <Login /> : <Home />}
-          </Route>
-          <Route path="/profile">
-            {session ? <Profile key={session.user.id} session={session} /> : <Login />}
-          </Route>
+          <Route path="/" element={!session ? <Login /> : <HomePage />} />
+          <Route path="/profile" element={session ? <Profile key={session.user.id} session={session} /> : <Login />} />
         </Routes>
       </Router>
     </div>
