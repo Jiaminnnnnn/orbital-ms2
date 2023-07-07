c4adafe9cdb220a6b0915abeb6e61721ad00747b
Jiamin <267354491@qq.com>
2023-07-08 00:33:06 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -1,14 +1,16 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './supabaseClient'
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
 import { Container, Heading, Text, Input, Button, Flex, Box, ChakraProvider, extendTheme, CSSReset, Stack } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 
 const customTheme = extendTheme({
   styles: {
-    body: {
-    bg: 'blue.300',
+    global: {
+      body: {
+        bg: 'blue.300',
+      }
     }
-  }, 
+  },
   fonts: {
     body: 'Georgia, sans-serif',
     heading: 'Georgia, serif',
@@ -16,58 +18,58 @@ const customTheme = extendTheme({
 });
 
 export default function Login() {
-  const [loading, setLoading] = useState(false)
-  const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
   const [isSent, setIsSent] = useState(false);
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithOtp({ email })
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
 
     if (error) {
-      alert(error.error_description || error.message)
+      alert(error.error_description || error.message);
     } else {
-      alert('Check your email for the login link!')
-      navigate('/home')
+      alert('Check your email for the login link!');
+      navigate('/home');
     }
-    setLoading(false)
+    setLoading(false);
   }
-  
+
   return (
     <ChakraProvider theme={customTheme}>
-    <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md">
-    <CSSReset />
-      <Flex justifyContent="center" alignItems="center" height="100%">
+      <Stack boxShadow="md" bg="whiteAlpha.900" p="20" rounded="md">
+        <CSSReset />
+        <Flex justifyContent="center" alignItems="center" height="100%">
           <Container>
             <Heading my="30px" color="blue.300">Stay The Course</Heading>
             <Flex flexDirection="column" alignItems="center">
-                <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">Sign in via magic link with your email below</Text>
-              </Flex>  
-        <form className="form-widget" onSubmit={handleLogin} style={{ textAlign: 'center' }}>
-        <Box marginBottom="20px" width="100%">
-          <div>
-            <Input
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          </Box>
-          <div>
-            <Button className={'button block'} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
-            </Button>
-          </div>
-        </form>
-        </Container>
+              <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">Sign in via magic link with your email below</Text>
+            </Flex>
+            <form className="form-widget" onSubmit={handleLogin} style={{ textAlign: 'center' }}>
+              <Box marginBottom="20px" width="100%">
+                <div>
+                  <Input
+                    className="inputField"
+                    type="email"
+                    placeholder="Your email"
+                    value={email}
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </Box>
+              <div>
+                <Button className={'button block'} disabled={loading}>
+                  {loading ? <span>Loading</span> : <span>Send magic link</span>}
+                </Button>
+              </div>
+            </form>
+          </Container>
         </Flex>
-        </Stack>
-        </ChakraProvider>
-  )
+      </Stack>
+    </ChakraProvider>
+  );
 }
