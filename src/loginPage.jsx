0a73a393134ceb22f7ce8d5189138e0463ae47cf
Jiamin <267354491@qq.com>
2023-07-07 09:57:24 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -28,17 +28,13 @@
     }
 
     return (
-      <ChakraProvider theme={customTheme}>
-        <CSSReset />
-          <Flex justifyContent="center" alignItems="center" height="100%">
-              <Container>
-                <Heading my="30px" color="blue.300">Stay The Course</Heading>
-                <Flex flexDirection="column" alignItems="center">
-                  <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">Sign in via magic link with your email below</Text>
-                </Flex>  
-                  <form className="form-widget" onSubmit={handleLogin}>
-                  <Box marginBottom="20px" width="100%">
-              <Input
+      <div className="row flex flex-center">
+        <div className="col-6 form-widget">
+          <h1 className="header">Supabase + React</h1>
+          <p className="description">Sign in via magic link with your email below</p>
+          <form className="form-widget" onSubmit={handleLogin}>
+            <div>
+              <input
                 className="inputField"
                 type="email"
                 placeholder="Your email"
@@ -46,13 +42,14 @@
                 required={true}
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </Box>
-              <Button className={'button block'} disabled={loading}>
+            </div>
+            <div>
+              <button className={'button block'} disabled={loading}>
                 {loading ? <span>Loading</span> : <span>Send magic link</span>}
-              </Button>
+              </button>
+            </div>
           </form>
-              </Container>
-          </Flex>
-      </ChakraProvider>
+        </div>
+      </div>
     )
   }
