2704e4f88b7ce086d47b7074a10b3fdba8673b07
Jiamin <267354491@qq.com>
2023-07-07 14:47:59 +0800

    new updates

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -1,6 +1,6 @@
 import { supabase } from './supabaseClient'
 import { useState } from 'react';
-import { Container, Heading, Text, Input, Button, Flex, Box, ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
+import { Container, Heading, Text, Input, Button, ChakraProvider, extendTheme, CSSReset, Center } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 
 const customTheme = extendTheme({
@@ -34,37 +34,29 @@ export default function Login() {
   return (
     <ChakraProvider theme={customTheme}>
       <CSSReset />
-      <Flex justifyContent="center" alignItems="center" height="100vh">
+      <Center height="100vh">
         <Container>
-          <Flex flexDirection="column" alignItems="center" justifyContent="center">
-            <Heading my="30px" color="blue.300">
-              Stay The Course
-            </Heading>
-            <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">
-              Sign in via magic link with your email below
-            </Text>
-            <form className="form-widget" onSubmit={handleLogin} style={{ textAlign: 'center' }}>
-              <Box marginBottom="20px" width="100%">
-                <div>
-                  <Input
-                    className="inputField"
-                    type="email"
-                    placeholder="Your email"
-                    value={email}
-                    required={true}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-              </Box>
-              <div>
-                <Button className={'button block'} disabled={loading}>
-                  {loading ? <span>Loading</span> : <span>Send magic link</span>}
-                </Button>
-              </div>
-            </form>
-          </Flex>
+          <Heading my="30px" color="blue.300">
+            Stay The Course
+          </Heading>
+          <Text fontWeight="bold" marginBottom="10px" fontFamily="heading">
+            Sign in via magic link with your email below
+          </Text>
+          <form className="form-widget" onSubmit={handleLogin}>
+            <Input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button className={'button block'} disabled={loading} mt="20px">
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            </Button>
+          </form>
         </Container>
-      </Flex>
+      </Center>
     </ChakraProvider>
   );
 }
