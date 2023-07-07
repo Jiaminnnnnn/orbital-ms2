d226632ada853432be404b6ac356139ee9d1e768
Jiamin <267354491@qq.com>
2023-07-07 14:57:16 +0800

    new updates

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import { Text, Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <Text>Dashboard</Text>
       <Link to="/profile">Profile</Link>
     </div>
   );
