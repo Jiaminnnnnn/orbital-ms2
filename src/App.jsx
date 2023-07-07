7ae86b4eda40d80e9f7e5280b247aea2c3a6103c
Jiamin <267354491@qq.com>
2023-07-07 12:57:30 +0800

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './loginPage';
 import Profile from './profilePage';
@@ -19,6 +21,15 @@ function App() {
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    </div>
+  )
+}
+
+export default App
+/*
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <Router>
         <Routes>
           <Route path="/" element={!session ? <Login /> : <Home />} />
@@ -26,7 +37,8 @@ function App() {
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+*/
